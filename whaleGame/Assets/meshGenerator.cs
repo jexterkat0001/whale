341c015d9d4d0fada839b1b378d2a24b9313e287
jexterkat0001 <158988912+jexterkat0001@users.noreply.github.com>
2024-03-26 17:05:16 -0700

    Finished the first version of whale system generation

--- whaleGame/Assets/meshGenerator.cs
+++ whaleGame/Assets/meshGenerator.cs
@@ -2,77 +2,91 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(MeshFilter))]
 public class meshGenerator : MonoBehaviour
 {
-    public ParticleSystem particleSystem;
+    public GameObject whaleTilePrefab;
 
-    Mesh mesh;
-
+    private int width = 100;
+    private int height = 100;
     [SerializeField]
-    private int width = 20;
+    private float noiseStretch;
     [SerializeField]
-    private int height = 20;
+    private float threshold1;
     [SerializeField]
-    private Color color;
-
-    Vector3[] vertices;
-    int[] triangles;
+    private float threshold2;
+    [SerializeField]
+    private Color color1;
+    [SerializeField]
+    private Color color2;
 
     // Start i  called before the first frame update
     void Start()
     {
-        mesh = new Mesh();
-        GetComponent<MeshFilter>().mesh = mesh;
-        GetComponent<MeshRenderer>().material.color = color;
-        var shape = particleSystem.shape;
-        shape.mesh = mesh;
-
-        CreateShape();
-        UpdateMesh();
+        createWhaleSystem(0,0);
     }
 
-    void Update(){GetComponent<MeshRenderer>().material.color = color;}
-
-    void CreateShape()
+    public void createWhaleSystem(float xOffset, float yOffset)
     {
-        vertices = new Vector3[(width+1) * (height+1)];
+        Instantiate(whaleTilePrefab, transform.position, transform.rotation, this.transform);
+        Transform whaleTile = transform.GetChild(transform.childCount-1);
+
+        Mesh outerMesh = createMesh(xOffset,yOffset,threshold1,threshold2);
+        Transform whaleSystemOuter = whaleTile.transform.GetChild(0);
+        whaleSystemOuter.GetComponent<MeshFilter>().mesh = outerMesh;
+        whaleSystemOuter.GetComponent<MeshRenderer>().material.color = color1;
+        var whaleSystemOuterShape = whaleSystemOuter.GetComponent<ParticleSystem>().shape;
+        whaleSystemOuterShape.mesh = outerMesh;
+        /*
+        Mesh innerMesh = createMesh(xOffset,yOffset,threshold2,1f);
+        Transform whaleSystemInner = whaleTile.transform.GetChild(1);
+        whaleSystemInner.GetComponent<MeshFilter>().mesh = innerMesh;
+        whaleSystemInner.GetComponent<MeshRenderer>().material.color = color2;
+        var whaleSystemInnerShape = whaleSystemInner.GetComponent<ParticleSystem>().shape;
+        whaleSystemInnerShape.mesh = innerMesh;*/
+    }
+
+    Mesh createMesh(float xOffset, float yOffset, float lowerThreshold, float upperThreshold)
+    {
+        Vector3[] vertices = new Vector3[(width+1) * (height+1)];
+        List<int> triangles = new List<int>();
+
         for(int i = 0, y = 0; y <= height; y++)
         {
             for(int x = 0; x <= width; x++)
             {
-                vertices[i] = new Vector3(x,y,0);
+                vertices[i] = new Vector3(x + xOffset,y + yOffset,0);
                 i++;
             }
         }
 
-        triangles = new int[width*height*6];
-        int index = 0;
-        for(int vertex = 0, y = 0; y < height; y++)
+        for(int i = 0, vertex = 0, y = 0; y < height; y++)
         {
-            for(int x = 0; x < width; x++)
+            for(int x = 0; x < width; x++, i+=6)
             {
-                triangles[index] = vertex;
-                triangles[index+1] = vertex + width + 1;
-                triangles[index+2] = vertex + 1;
-                triangles[index+3] = vertex + 1;
-                triangles[index+4] = vertex + width + 1;
-                triangles[index+5] = vertex + width + 2;
-                index += 6;
+                float value = Mathf.PerlinNoise(vertices[vertex].x * noiseStretch * Mathf.PI + 1000000, vertices[vertex].y * noiseStretch * Mathf.PI + 1000000);
+                if(value > lowerThreshold && value < upperThreshold)
+                {
+                    triangles.Add(vertex);
+                    triangles.Add(vertex + width + 1);
+                    triangles.Add(vertex + 1);
+                    triangles.Add(vertex + 1);
+                    triangles.Add(vertex + width + 1);
+                    triangles.Add(vertex + width + 2);
+                }
                 vertex++;
             }
             vertex++;
         }
-        
-        
-    }
 
-    void UpdateMesh()
-    {
-        mesh.Clear();
-
+        Mesh mesh = new Mesh();
         mesh.vertices = vertices;
-        mesh.triangles = triangles;
+        int[] trianglesArray = new int[triangles.Count];
+        for(int i = 0; i < triangles.Count; i++)
+        {
+            trianglesArray[i] = triangles[i];
+        }
+        mesh.triangles = trianglesArray;
+        return mesh;
     }
     
 }
